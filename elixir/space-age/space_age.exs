2dfa168a2ee317eb5c884b71f6b9f742f61287e2
Jordan Adams <hi@jordanadams.co.uk>
2016-10-08 11:16:24 +0100

    Completed Elixir's "Space Age"

---
+++ elixir/space-age/space_age.exs
@@ -0,0 +1,14 @@
+defmodule SpaceAge do
+  @planets %{
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    earth: 1,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
+  }
+
+  def age_on(planet, seconds), do: seconds / (@planets[planet] * 31557600)
+end
