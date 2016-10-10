8968c816df9b30f3aacc16f5d3346c44ae42cf47
Jordan Adams <hi@jordanadams.co.uk>
2016-10-10 21:47:35 +0100

    Completed isogram

--- elixir/isogram/isogram.exs
+++ elixir/isogram/isogram.exs
@@ -4,7 +4,14 @@ defmodule Isogram do
   """
   @spec isogram?(String.t) :: boolean
   def isogram?(sentence) do
-
+    String.replace(sentence, ~r/\P{L}/u, "")
+    |> String.downcase
+    |> String.codepoints
+    |> Enum.sort
+    |> check_isogram
   end
 
+  defp check_isogram([]), do: true
+  defp check_isogram([x, x | _]), do: false
+  defp check_isogram([_ | rest]), do: check_isogram(rest)
 end
