fbffa507875f747257abd28c09412cf6db142f14
Jordan Adams <hi@jordanadams.co.uk>
2017-08-03 21:15:26 +0100

    Completes erlang/bob

--- erlang/bob/src/bob.erl
+++ erlang/bob/src/bob.erl
@@ -2,31 +2,54 @@
 
 -export([response_for/1, test_version/0]).
 
-response_for(Input) ->
-  IsQuestion = is_question(Input),
-  IsSilence  = is_silence(Input),
-  IsShouting = is_shouting(Input),
+response_for([]) -> "Fine. Be that way!";
 
-  if IsShouting -> "Whoa, chill out!"
-   ; IsQuestion -> "Sure."
-   ; IsSilence  -> "Fine. Be that way!"
-   ; true       -> "Whatever."
-  end.
+response_for(Statement) ->
+    [LastCharacter | _] = lists:reverse(Statement),
+    UpperCaseResult = is_uppercase(Statement),
+    StrippedResult = string:strip(Statement) =:= [],
 
-is_question(Input) ->
-  case re:run(Input, "\\?$") of
-    {match, _} -> true ;
-    _          -> false
-  end.
+    if
+        StrippedResult -> "Fine. Be that way!";
+        [LastCharacter] == "?" -> "Sure.";
+        UpperCaseResult == true -> "Whoa, chill out!";
+        true -> "Whatever."
+    end.
 
-is_silence(Input) ->
-  case re:run(Input, "^[\s]*$") of
-    {match, _} -> true ;
-    _          -> false
-  end.
+is_uppercase(Statement) ->
+    (string:to_upper(Statement) == Statement) andalso is_re_match(re:run(Statement, "[A-Z]+")).
 
-is_shouting(Input) ->
-  string:to_upper(Input) == Input.
+is_re_match({match,_}) -> true;
+is_re_match(_) -> false.
+
+% response_for(Input) ->
+%   IsQuestion = is_question(Input),
+%   IsSilence  = is_silence(Input),
+%   IsShouting = is_shouting(Input),
+%
+%   if IsSilence  -> "Fine. Be that way!"
+%    ; IsShouting -> "Whoa, chill out!"
+%    ; IsQuestion -> "Sure."
+%    ; true       -> "Whatever."
+%   end.
+%
+% is_question(Input) ->
+%   case re:run(Input, "\\?$") of
+%     {match, _} -> true ;
+%     _          -> false
+%   end.
+%
+% is_silence(Input) ->
+%   case re:run(Input, "^[\s]*$") of
+%     {match, _} -> true ;
+%     _          -> false
+%   end.
+%
+% is_shouting(Input) ->
+%   case re:run(Input, "^[^a-z]+$") of
+%     {match, _} -> true ;
+%     _          -> false
+%   end.
 
 test_version() ->
   1.
