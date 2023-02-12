f9b46691f6676f97353589e48d6baddb9ee7be52
Jordan Adams <hi@jordanadams.co.uk>
2023-02-12 16:32:31 +0000

    Completed rust/resistor-color

---
+++ rust/resistor-color/src/lib.rs
@@ -0,0 +1,40 @@
+use std::fmt::Display;
+
+use enum_iterator::{all, Sequence };
+use int_enum::IntEnum;
+
+#[repr(u32)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq, IntEnum, Sequence)]
+pub enum ResistorColor {
+    Black = 0,
+    Brown = 1,
+    Red = 2,
+    Orange = 3,
+    Yellow = 4,
+    Green = 5,
+    Blue = 6,
+    Violet = 7,
+    Grey = 8,
+    White = 9,
+}
+
+impl Display for ResistorColor {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        return write!(f, "{:?}", self);
+    }
+}
+
+pub fn color_to_value(color: ResistorColor) -> u32 {
+    return color.int_value();
+}
+
+pub fn value_to_color_string(value: u32) -> String {
+    return match ResistorColor::from_int(value) {
+        Ok(value) => value.to_string(),
+        Err(_) => String::from("value out of range")
+    }
+}
+
+pub fn colors() -> Vec<ResistorColor> {
+    return all().collect();
+}
